c8c87decf23a134adf930145ff16ff4d24d0c800
L00ka5z78 <110019733+L00ka5z78@users.noreply.github.com>
2023-09-18 09:01:48 +0000

    add jobb [] and conneted with html

--- app.py
+++ app.py
@@ -2,10 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data analyst',
+        'location': 'abu dhabi',
+        'salary': 'USD 1000'
+    },
+    {
+        'id': 2,
+        'title': 'Data scientist',
+        'location': 'warszawa',
+        'salary': 'USD 3000'
+    },
+    {
+        'id': 3,
+        'title': 'Data admin',
+        'location': 'oslo',
+        'salary': 'USD 2000'
+    },
+    {
+        'id': 4,
+        'title': 'Database engineer',
+        'location': 'zurich',
+        'salary': 'USD 1500'
+    },
+]
+
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
 
 print(__name__)
