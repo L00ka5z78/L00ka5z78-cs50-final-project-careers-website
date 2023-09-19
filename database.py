4525d418150c3c22cf18a18c370d3675358fbd17
L00ka5z78 <110019733+L00ka5z78@users.noreply.github.com>
2023-09-19 08:26:44 +0000

    db connection issues 19.09.23 10:26

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine, text
 
-engine = create_engine(
-    "mysql+pymysql://g676u90rbk5cu76gon38:pscale_pw_uLlW7yDthDtmrrLyJYzuobRl8cprzzsADRRaMDOgsno@aws.connect.psdb.cloud/careers?charset=utf8mb4"
-)
+db_connection_string = "mysql+pymysql://g676u90rbk5cu76gon38:pscale_pw_uLlW7yDthDtmrrLyJYzuobRl8cprzzsADRRaMDOgsno@aws.connect.psdb.cloud/careers?charset=utf8mb4"
+
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                           "ssl_ca": "/etc/ssl/ca.pem"
+                       }})
 
 with engine.connect() as conn:
   result = conn.execute(text("select * from jobs"))
+  print(result.all())
