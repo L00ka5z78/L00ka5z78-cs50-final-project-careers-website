d50b4f076e45579b278e965612f0f1b8be9dd556
L00ka5z78 <110019733+L00ka5z78@users.noreply.github.com>
2023-09-20 10:39:00 +0000

    db connection issue fixed 20.09.23 12:38

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine, text
 
-db_connection_string = "mysql+pymysql://g676u90rbk5cu76gon38:pscale_pw_uLlW7yDthDtmrrLyJYzuobRl8cprzzsADRRaMDOgsno@aws.connect.psdb.cloud/careers?charset=utf8mb4"
+db_connection_string = "mysql+pymysql://gazqn9tgfbtv60bmtiao:pscale_pw_GDtxRV1WOomRqwWqqQNRLOYkSUyHPjiCgjZEs8wKZCU@aws.connect.psdb.cloud/careers?charset=utf8mb4"
+
+# "mysql+pymysql://g676u90rbk5cu76gon38:pscale_pw_uLlW7yDthDtmrrLyJYzuobRl8cprzzsADRRaMDOgsno@aws.connect.psdb.cloud/careers?charset=utf8mb4"
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
