efb1742e6a7537eef4b06de2536d4bed3a602d15
L00ka5z78 <110019733+L00ka5z78@users.noreply.github.com>
2023-09-21 11:47:22 +0000

    secret added  to protect sensitive data 21.09.23 13:47

--- database.py
+++ database.py
@@ -1,14 +1,20 @@
 from sqlalchemy import create_engine, text
-
-db_connection_string = "mysql+pymysql://gazqn9tgfbtv60bmtiao:pscale_pw_GDtxRV1WOomRqwWqqQNRLOYkSUyHPjiCgjZEs8wKZCU@aws.connect.psdb.cloud/careers?charset=utf8mb4"
-
-# "mysql+pymysql://g676u90rbk5cu76gon38:pscale_pw_uLlW7yDthDtmrrLyJYzuobRl8cprzzsADRRaMDOgsno@aws.connect.psdb.cloud/careers?charset=utf8mb4"
+import os
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+# db_connection_string = "mysql+pymysql://gazqn9tgfbtv60bmtiao:pscale_pw_GDtxRV1WOomRqwWqqQNRLOYkSUyHPjiCgjZEs8wKZCU@aws.connect.psdb.cloud/careers?charset=utf8mb4"
+# db_connection_string = "mysql+pymysql://2a9gyc7yrqtbzsfmj854:pscale_pw_B9P9oKlxEn1DULWztsWEQBO3TVV7EFUINxOeGYBq9bh@aws.connect.psdb.cloud/careers?charset=utf8mb4"
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
-                           "ssl_ca": "/etc/ssl/ca.pem"
+                           "ssl_ca": "/etc/ssl/cert.pem"
                        }})
 
-with engine.connect() as conn:
-  result = conn.execute(text("select * from jobs"))
-  print(result.all())
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+
+    for row in result.all():
+      jobs.append(dict(row._mapping))
+    return jobs
